993373c00a5ea13abbec10b41268e3c7f4e61de2
Oualha Bilel <oualha@ensta.fr>
2017-12-15 12:40:28 +0100

    test PiocheInitiale

--- src/test/formation/xp/TestPiocheInitiale.java
+++ src/test/formation/xp/TestPiocheInitiale.java
@@ -1,11 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package formation.xp;
 
-import org.junit.Test;
 
 import junit.framework.TestCase;
 
-public class TestPiocheInitiale extends TestCase {
-       Partie game = new Partie();
-       
-       game.init()
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
+public class TestPiocheInitiale extends TestCase{
+    
+    
+
+    /**
+     * Test of init method, of class Partie.
+     */
+    @Test
+    public void testPiocheInitiale() {
+        
+        int nbrJoueur =2;
+        String[] nomJoueur ={"Majd","Bilel"};
+        int[] montantJoueur ={100,200};
+        Partie instance = new Partie();
+        instance.init(nbrJoueur, nomJoueur, montantJoueur);
+        Carte[] main = instance.getJoueurs().get(0).getMain();
+        assertEquals(2, main.length);
+
+    }
 }
