57532397c15089403a4c59a8ada100dbde68c86f
Rebai Majdeddine <majdrebai2007@gmail.com>
2017-12-15 10:58:40 +0100

    version 1.3

--- src/java/formation/xp/Main.java
+++ src/java/formation/xp/Main.java
@@ -2,6 +2,10 @@ package formation.xp;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("ok");
+		System.out.println("Bienvenu au jeu Poker! ");
+                //initialisatiion d'une partie avec une miseTotale=0 et derniereMise=0 et nobre de joueur introduit par les joueurs
+                System.out.println("Veuillez saisir le nombre de joueurs:");
+                
+                Partie partie=new Partie(0,0,0);
 	}
 }
--- src/java/formation/xp/Partie.java
+++ src/java/formation/xp/Partie.java
@@ -4,6 +4,9 @@
  */
 package formation.xp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author rebai
@@ -12,4 +15,56 @@ public class Partie {
     private Carte[] tapis=new Carte[3];
     private int miseTotale;
     private int dernierMise;
+    private int nbrJoueur;
+    private List<Joueur> joueurs = new ArrayList<Joueur>();
+
+    public void setJoueurs(List<Joueur> joueurs) {
+        this.joueurs = joueurs;
+    }
+
+    public List<Joueur> getJoueurs() {
+        return joueurs;
+    }
+    
+
+    public void setDernierMise(int dernierMise) {
+        this.dernierMise = dernierMise;
+    }
+
+    public void setMiseTotale(int miseTotale) {
+        this.miseTotale = miseTotale;
+    }
+
+    public void setNbrJoueur(int nbrJoueur) {
+        this.nbrJoueur = nbrJoueur;
+    }
+
+    public void setTapis(Carte[] tapis) {
+        this.tapis = tapis;
+    }
+
+    public int getDernierMise() {
+        return dernierMise;
+    }
+
+    public int getMiseTotale() {
+        return miseTotale;
+    }
+
+    public int getNbrJoueur() {
+        return nbrJoueur;
+    }
+
+    public Carte[] getTapis() {
+        return tapis;
+    }
+
+    public Partie(int miseTotale, int dernierMise, int nbrJoueur) {
+        this.miseTotale = miseTotale;
+        this.dernierMise = dernierMise;
+        this.nbrJoueur = nbrJoueur;
+    }
+
+   
+    
 }
